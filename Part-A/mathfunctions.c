30042f0d712968f8a7331d1cd72a7d101181544c
Shivakumar-001 <shivagana2004@gmail.com>
2024-10-19 23:31:56 +0530

    Math functions implementation

---
+++ Part-A/mathfunctions.c
@@ -0,0 +1,16 @@
+//PROGRAM TO DEMONSTRATE MATH FUNCTIONS
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	clrscr();
+	printf("Mathematical Functions\n");
+	printf("Sin(45)=%f\n",sin(45));
+	printf("Power(4,2)=%1f\n",pow(4,2));
+	printf("Sqrt(4)=%f\n",sqrt(4));
+	printf("Floor(4.3)=%f\n",floor(4.2));
+	printf("Ceil(4.3)=%f\n",ceil(4.3));
+	printf("Log(5)=%f",log(5));
+	getch();
+}
+
